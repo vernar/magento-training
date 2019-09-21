0ce95556abb1228ea87f323c9060d7a9b97f1149
vernar <arbuzov.mail@gmail.com>
2019-09-21 12:12:37 +0300

    1.3.1 create new module, check dependency

---
+++ app/code/Unit1/Test/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * File: registration.php.
+ * User: dmitry
+ * Date: 21.09.2019
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE,
+    'Unit1_Test',
+    __DIR__
+);
---
+++ app/code/Unit1/Test2/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * File: registration.php.
+ * User: dmitry
+ * Date: 21.09.2019
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE,
+    'Unit1_Test2',
+    __DIR__);
